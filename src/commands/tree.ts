9babc0810f2b2294420e8c8d8ccea9fcf0032865
Tim O'Brien <tobrien@discursive.com>
2026-01-19 20:28:08 -0800

    fix(tree): remove invalid --skip-link-cleanup option from publish command

--- src/commands/tree.ts
+++ src/commands/tree.ts
@@ -2600,7 +2600,8 @@ export const execute = async (runConfig: Config): Promise<string> => {
 
             // Publish command options (pass self-reflection - publish reads other release config from config file)
             if (builtInCommand === 'publish') {
-                commandSpecificOptions += ' --skip-link-cleanup';
+                // Note: --skip-link-cleanup option removed as it's not supported in publish command
+                // commandSpecificOptions += ' --skip-link-cleanup';
                 if (runConfig.release?.selfReflection) {
                     commandSpecificOptions += ' --self-reflection';
                 }
